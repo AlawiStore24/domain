4ca6f3e1f6d660803882ab2aeb4ac5a3ebcedeb1
AlawiStore24 <159411067+AlawiStore24@users.noreply.github.com>
2024-02-13 14:50:22 +0700

    Update pointingtes.sh

--- pointingtes.sh
+++ pointingtes.sh
@@ -67,5 +67,45 @@ echo -e "┌──────────────────────�
 echo -e "│   POINTING DOMAIN KE CLOUDFLARE SELESAI  │"
 echo -e "└──────────────────────────────────────────┘"
 echo -e "Pointing ${SUB_DOMAIN1} selesai..."
-el
+elif [ $host -eq 2 ]
+then
+  clear
+  echo -e "------>  xxx.${DOMAIN2}  <------"
+  echo -e ""
+  echo -e "isi xxx dengan subdomain kamu"
+  echo -e ""
+  read -rp "masukkan subdomain kamu: " -e sub2
+  read -rp "masukkan ip vps kamu   : " -e ip2
+
+set -euo pipefail
+IP2=${ip2};
+
+echo "Updating DNS for ${SUB_DOMAIN2}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN2}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN2}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN2}'","content":"'${IP2}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN2}'","content":"'${IP2}'","ttl":120,"proxied":false}')
+clear
+
+echo -e ""
+echo -e "┌──────────────────────────────────────────┐"
+echo -e "│   POINTING DOMAIN KE CLOUDFLARE SELESAI  │"
+echo -e "└──────────────────────────────────────────┘"
+echo -e "Pointing ${SUB_DOMAIN2} selesai..."
 fi
